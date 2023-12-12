1f8675bdb04c3b2278fdeb78ec6bec29b3cbf116
yosnak13 <yosnak13@gmail.com>
2023-12-13 08:41:46 +0900

    generate show action in evetns_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/events', to: 'events#index'
+  get '/events/:id', to: 'events#show'
 end
